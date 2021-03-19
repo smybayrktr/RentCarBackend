c2df28c1564b8abb0daff615e29142e44bc5138d
HP <HP@DESKTOP-LSDB34O>
2021-03-19 15:57:44 +0300

    İkinci Aşama

--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -20,5 +20,27 @@ namespace Business.Concrete
         {
             return _carDal.GetAll();
         }
+
+        public List<Car> GetCarsByBrandId(int brandId)
+        {
+            return _carDal.GetAll(p => p.BrandId == brandId);
+        }
+
+        public List<Car> GetCarsByColorId(int colorId)
+        {
+            return _carDal.GetAll(p => p.ColorId == colorId);
+        }
+
+        public void Verification(Car car)
+        {
+            if (car.Name.Length>=2 && car.DailyPrice>=0)
+            {
+                _carDal.Add(car);
+            }
+            else
+            {
+                Console.WriteLine("Araba adı 2 harften uzun veya günlük fiyatı 0 dan büyük olmalıdır.");
+            }
+        }
     }
 }
--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,5 +1,7 @@
 ﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
 using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
 using System;
 
 namespace ConsoleUI
@@ -8,12 +10,42 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
+            //InMemoryTest();
+            //EfCarDalTest();
+            EfCarDalTest2();
+
+        }
+
+        private static void EfCarDalTest2()
+        {
+            CarManager carManager = new CarManager(new EfCarDal());
+            Car car = new Car();
+            car.Name = "Yeşil Araba";
+            car.BrandId = 5;
+            car.ColorId = 2;
+            car.DailyPrice = 0;
+            car.Description = "2010 model yeni bakımdan çıkmış araba";
+            car.Id = 6;
+            car.ModelYear = 2010;
+            carManager.Verification(car);
+        }
+
+        private static void EfCarDalTest()
+        {
+            CarManager carManager = new CarManager(new EfCarDal());
+            foreach (var car in carManager.GetCarsByBrandId(1))
+            {
+                Console.WriteLine(car.Name);
+            }
+        }
+
+        private static void InMemoryTest()
+        {
             CarManager carManager = new CarManager(new InMemoryCarDal());
             foreach (var car in carManager.GetAll())
             {
                 Console.WriteLine(car.Name);
             }
-
         }
     }
 }
